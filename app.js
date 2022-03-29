c12c98544c5600c5f687b386e4d935a24d4a111c
siarhei.hardzeichyk <S_G89@mail.ru>
2022-03-29 21:32:55 +0300

    rework code, added get usd rate from onliner page, added update usd price in products and offers list when user select filter or clock "show more"

--- app.js
+++ app.js
@@ -1,38 +1,64 @@
 let usdToBynRate = null;
-let lastUpdateDate = null;
+let preparing = false;
 
-const updateTimeInterval = 10 // in minutes;
-const maxUpdateTimeInterval = 720 // in minutes;
-const bestrateApi = '/sdapi/kurs/api/bestrate?currency=USD&type=nbrb';
-const notResonUpdateDates = [6, 0]; // Saturday and Sunday
-const startTradingHour = 10; // start tradin in 10:00
-const endTradingHour = 13; // end trading in 13:00
-const priceContainerSelectors = [
+const usdToBynRateObserverSelector = '.js-currency-amount';
+const offersListGroupObserverSelector = '.offers-list__group';
+const catalogObserverSelector = '#schema-products';
+const staticPriceContainerSelectors = [
 	'.offers-description__details .offers-description__price-group .offers-description__price',
 	'.product-aside__offers-list .product-aside__offers-part .product-aside__description .product-aside__link.js-short-price-link',
-	'.offers-list__group .offers-list__item .offers-list__part_price .offers-list__description',
+	'.offers-list__group .offers-list__item .offers-list__part_price .offers-list__description_alter',
 	'.catalog-offers .catalog-offers__list .catalog-offers__item .catalog-offers__price .catalog-offers__link',
 	'.schema-product .schema-product__part .schema-product__price .schema-product__price-value.js-product-price-link',
 	'.schema-product .schema-product__part .schema-product__price .schema-product__price-value span',
 	'.product-recommended .product-recommended__list .product-recommended__item .product-recommended__price .product-recommended__link'
 ];
 
-fillWebData();
+start();
 
-async function fillWebData() {
-	await getDataFromStorage();
-
-	if (!usdToBynRate) {
-		return;
-	}
-
-	setTimeout(() => {
-		fillUsdPrice();
-	}, 1000);
+function start() {
+	mutationObserver(usdToBynRateObserverSelector);
+	mutationObserver(offersListGroupObserverSelector);
+	mutationObserver(catalogObserverSelector);
 }
 
-function fillUsdPrice() {
-	priceContainerSelectors.forEach(priceContainerSelector => {
+function mutationObserver(selector) {
+	const observer = new MutationObserver((mutations) => {
+		mutations.forEach((mutation) => {
+			if (selector === usdToBynRateObserverSelector && mutation.addedNodes.length && mutation.addedNodes[0].nodeName === '#text') {
+				usdToBynRate = getPriceInNumber(mutation.addedNodes[0].data);
+			}
+
+			if (preparing) {
+				return;
+			}
+
+			preparing = true;
+
+			setTimeout(() => {
+				preparing = false;
+				fillStaticWebData(staticPriceContainerSelectors);
+			}, 1000);
+		});
+	});
+
+	const container = document.querySelector(selector);
+
+	if (container) {
+		observer.observe(container, { childList: true });
+	}
+}
+
+function fillStaticWebData(selectors) {
+		if (!usdToBynRate) {
+			return;
+		}
+	
+		fillUsdPrice(selectors);
+}
+
+function fillUsdPrice(selectors) {
+	selectors.forEach(priceContainerSelector => {
 		const priceContainers = document.querySelectorAll(priceContainerSelector);
 
 		if (!priceContainers?.length) {
@@ -40,77 +66,20 @@ function fillUsdPrice() {
 		}
 
 		for (let priceContainer of priceContainers) {
+			if (priceContainer.innerText.includes('$')) {
+				continue;
+			}
+
 			priceContainer.innerText = `${priceContainer.innerText} \n ${getUsdPrice(priceContainer.innerText)} $`;
 		}
 	});
 }
 
 function getUsdPrice(priceString) {
-	return (getBynPrice(priceString) / usdToBynRate).toFixed(2);
+	return (getPriceInNumber(priceString) / usdToBynRate).toFixed(2);
 }
 
-function getBynPrice(priceString) {
+function getPriceInNumber(priceString) {
 	const originalPrice = priceString.replace(/[^,\d]/g, '');
 	return +originalPrice.split(',').join('.');
-}
-
-async function updateUsdToByn() {
-	await fetch(`https://${window.location.hostname}${bestrateApi}`)
-		.then(response => response.json())
-		.then(result => setUsdToByn(result));
-}
-
-function setUsdToByn(result) {
-	return new Promise(resolve => {
-		if (!result) {
-			resolve();
-		}
-
-		usdToBynRate = +result.amount.split(',').join('.');
-		lastUpdateDate = getCurrentTimeInMinutes();
-		saveDataToStorage();
-		resolve();
-	});
-}
-
-function saveDataToStorage() {
-	chrome.storage.local.set({usdToBynRate});
-	chrome.storage.local.set({lastUpdateDate});
-}
-
-function getDataFromStorage() {
-	return new Promise(resolve => {
-		chrome.storage.local.get(['usdToBynRate', 'lastUpdateDate'], function(result) {
-		  usdToBynRate = result.usdToBynRate;
-		  lastUpdateDate = result.lastUpdateDate;
-
-		  if (!isNeedUpdateData()) {
-			  resolve();
-			  return;
-		  }
-
-		  resolve(updateUsdToByn());
-		});
-	});
-}
-
-function isNeedUpdateData() {
-	return !usdToBynRate
-		|| !lastUpdateDate
-		|| isResonToUpdate();
-}
-
-function isResonToUpdate() {
-	const date = new Date();
-	const currentHour = date.getHours();
-
-	return !notResonUpdateDates.includes(date.getDay())
-		&& currentHour > startTradingHour
-		&& currentHour < endTradingHour
-		&& getCurrentTimeInMinutes() - lastUpdateDate > updateTimeInterval
-		|| getCurrentTimeInMinutes() - lastUpdateDate > maxUpdateTimeInterval;
-}
-
-function getCurrentTimeInMinutes() {
-	return new Date().getTime() / 1000;
 }
